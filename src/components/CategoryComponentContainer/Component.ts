46fc3efb5902fa8cc069ef6b8ff66f9c1c2ba0c2
Jad <jad.saad@aot-technologies.com>
2022-11-16 14:01:55 -0800

    Add constant for string and remove unnecessary condition

--- src/components/CategoryComponentContainer/Component.ts
+++ src/components/CategoryComponentContainer/Component.ts
@@ -8,101 +8,103 @@ import { CHECKBOXES_VC } from '../CheckBoxesVC/Component';
 
 const ID = 'categorycommentcontainer';
 const DISPLAY = 'Category Comment';
+const OTHER = 'other';
 
 export default class Component extends (ParentComponent as any) {
-    static schema(...extend) {
-        return ParentComponent.schema(
-            {
-                label: DISPLAY,
-                tableView: false,
-                key: ID,
-                input: true,
-                components: [
-                    {
-                        label: 'We would like to learn more about your concerns, thoughts, and idea in relation to the items you selected.',
-                        key: 'textareavc',
-                        type: 'textareavc',
-                        rows: 3,
-                        wysiwyg: false,
-                        editor: '',
-                        spellcheck: true,
-                        fixedSize: true,
-                        inputFormat: 'plain',
-                        validate: {
-                            minWords: '',
-                            maxWords: '',
-                        },
-                        logic: [
-                            {
-                                name: 'componentChange event',
-                                trigger: {
-                                    type: 'event',
-                                    event: 'componentChange',
-                                },
-                                actions: [
-                                    {
-                                        name: 'update value components',
-                                        type: 'customAction',
-                                        customAction:
-                                            `const change = result[0]\nif(change && change.component.key === '${CHECKBOXES_VC}')` +
-                                            ' {\n  const vcs = change.component.values.filter(vc => change.value[vc.value] && vc.value !== "other")\n' +
-                                            '  instance.valueComponents = [...vcs]\n  instance.redraw()\n}',
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                    {
-                        label: 'Tell us more about the other concerns that you have.',
-                        key: 'textareavc1',
-                        customConditional: `show = data && data['${CHECKBOXES_VC}'] && data['${CHECKBOXES_VC}']['other']`,
-                        type: 'textareavc',
-                        rows: 3,
-                        wysiwyg: false,
-                        editor: '',
-                        spellcheck: true,
-                        fixedSize: true,
-                        inputFormat: 'plain',
-                        validate: {
-                            minWords: '',
-                            maxWords: '',
-                        },
-                        logic: [
-                            {
-                                name: 'componentChange event',
-                                trigger: {
-                                    type: 'event',
-                                    event: 'componentChange',
-                                },
-                                actions: [
-                                    {
-                                        name: 'update value components',
-                                        type: 'customAction',
-                                        customAction:
-                                            `const change = result[0]\nif(change && change.component.key === '${CHECKBOXES_VC}')` +
-                                            ' {\n  const vcs = change.component.values.filter(vc => vc.value === "other")\n' +
-                                            '  instance.valueComponents = [...vcs]\n  instance.redraw()\n}',
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                ],
+  static schema(...extend) {
+    return ParentComponent.schema(
+      {
+        label: DISPLAY,
+        tableView: false,
+        key: ID,
+        input: true,
+        components: [
+          {
+            label:
+              "We would like to learn more about your concerns, thoughts, and idea in relation to the items you selected.",
+            key: "textareavc",
+            type: "textareavc",
+            rows: 3,
+            wysiwyg: false,
+            editor: "",
+            spellcheck: true,
+            fixedSize: true,
+            inputFormat: "plain",
+            validate: {
+              minWords: "",
+              maxWords: "",
             },
-            ...extend,
-        );
-    }
+            logic: [
+              {
+                name: "componentChange event",
+                trigger: {
+                  type: "event",
+                  event: "componentChange",
+                },
+                actions: [
+                  {
+                    name: "update value components",
+                    type: "customAction",
+                    customAction:
+                      `const change = result[0]\nif(change && change.component.key === '${CHECKBOXES_VC}')` +
+                      ` {\n  const vcs = change.component.values.filter(vc => change.value[vc.value] && vc.value !== '${OTHER}')\n` +
+                      "  instance.valueComponents = [...vcs]\n  instance.redraw()\n}",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            label: "Tell us more about the other concerns that you have.",
+            key: "textareavc1",
+            customConditional: `show = data && data['${CHECKBOXES_VC}'] && data['${CHECKBOXES_VC}']['${OTHER}']`,
+            type: "textareavc",
+            rows: 3,
+            wysiwyg: false,
+            editor: "",
+            spellcheck: true,
+            fixedSize: true,
+            inputFormat: "plain",
+            validate: {
+              minWords: "",
+              maxWords: "",
+            },
+            logic: [
+              {
+                name: "componentChange event",
+                trigger: {
+                  type: "event",
+                  event: "componentChange",
+                },
+                actions: [
+                  {
+                    name: "update value components",
+                    type: "customAction",
+                    customAction:
+                      `const change = result[0]\nif(change && change.component.key === '${CHECKBOXES_VC}')` +
+                      ` {\n  const vcs = change.component.values.filter(vc => vc.value === '${OTHER}')\n` +
+                      "  instance.valueComponents = [...vcs]\n  instance.redraw()\n}",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      ...extend
+    );
+  }
 
-    public static editForm = editForm;
+  public static editForm = editForm;
 
-    static get builderInfo() {
-        return {
-            title: DISPLAY,
-            group: 'simple',
-            icon: 'pencil-square-o',
-            weight: 40,
-            documentation: Constants.DEFAULT_HELP_LINK,
-            schema: Component.schema(),
-        };
-    }
+  static get builderInfo() {
+    return {
+      title: DISPLAY,
+      group: "simple",
+      icon: "pencil-square-o",
+      weight: 40,
+      documentation: Constants.DEFAULT_HELP_LINK,
+      schema: Component.schema(),
+    };
+  }
 }
