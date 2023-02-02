faca4eb837777c21cf0dd8e93ecb48276525a31a
Jad <jad.saad@aot-technologies.com>
2023-02-02 09:18:49 -0800

    Chage Data label to Values

--- src/components/SimpleCheckboxes/Component.form.ts
+++ src/components/SimpleCheckboxes/Component.form.ts
@@ -6,51 +6,51 @@ import SimpleConditional from '../Common/Simple.edit.conditional';
 
 export default function (...extend) {
     return radioEditForm(
-        [
-            EditDisplay,
-            {
-                key: 'data',
-                ignore: true,
-            },
-            {
-                key: 'api',
-                ignore: true,
-            },
-            {
-                key: 'layout',
-                ignore: true,
-            },
-            {
-                key: 'conditional',
-                ignore: true,
-            },
-            {
-                key: 'validation',
-                ignore: true,
-            },
-            {
-                key: 'logic',
-                ignore: true,
-            },
-            {
-                label: 'Data',
-                key: 'customData',
-                weight: 10,
-                components: EditData,
-            },
-            {
-                label: 'Validation',
-                key: 'customValidation',
-                weight: 20,
-                components: EditValidation,
-            },
-            {
-                label: 'Conditional',
-                key: 'customConditional',
-                weight: 40,
-                components: SimpleConditional,
-            },
-        ],
-        ...extend,
+      [
+        EditDisplay,
+        {
+          key: "data",
+          ignore: true,
+        },
+        {
+          key: "api",
+          ignore: true,
+        },
+        {
+          key: "layout",
+          ignore: true,
+        },
+        {
+          key: "conditional",
+          ignore: true,
+        },
+        {
+          key: "validation",
+          ignore: true,
+        },
+        {
+          key: "logic",
+          ignore: true,
+        },
+        {
+          label: "Values",
+          key: "customData",
+          weight: 10,
+          components: EditData,
+        },
+        {
+          label: "Validation",
+          key: "customValidation",
+          weight: 20,
+          components: EditValidation,
+        },
+        {
+          label: "Conditional",
+          key: "customConditional",
+          weight: 40,
+          components: SimpleConditional,
+        },
+      ],
+      ...extend
     );
 }
