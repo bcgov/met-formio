1861ef1cac289e2bf6f76b80ddc973cb335e102c
Jad <jad.saad@aot-technologies.com>
2023-11-20 13:34:46 -0800

    Add target blank automatically to a tags in content

--- src/components/SimpleContent/Component.ts
+++ src/components/SimpleContent/Component.ts
@@ -24,6 +24,38 @@ export default class Component extends (ParentComponent as any) {
 
     public static editForm = editForm;
 
+    render() {
+        const div = super.render(
+            this.renderTemplate('html', {
+                tag: 'div',
+                attrs: [],
+                content: this.content,
+            }),
+        );
+
+        // Create a DOM parser to parse the HTML string into a document
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(div, 'text/html');
+
+        // Get all the <a> elements in the document
+        const links = doc.getElementsByTagName('a');
+        for (let i = 0; i < links.length; i++) {
+            const link = links[i];
+
+            // Set the target attribute to '_blank' to open in a new tab
+            link.setAttribute('target', '_blank');
+
+            // Add rel="noopener noreferrer" for security
+            link.setAttribute('rel', 'noopener noreferrer');
+        }
+
+        // Serialize the document back into an HTML string
+        const serializer = new XMLSerializer();
+        const updatedContent = serializer.serializeToString(doc);
+
+        return updatedContent;
+    }
+
     static get builderInfo() {
         return {
             title: DISPLAY,
